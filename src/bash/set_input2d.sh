a47e071ca409afeeb77bf3cfffa815d8471d52e7
lindsaywaldrop <waldrop@chapman.edu>
2022-05-14 13:36:15 -0700

    Fixing shell script for input2d

--- src/bash/set_input2d.sh
+++ src/bash/set_input2d.sh
@@ -19,10 +19,11 @@ for i in `seq 1 $numlines`;
 do
 # Sets Wo based on i
 number=$(awk -v var="$i" 'NR==var' number.txt)
+numspec=${Species}_${number}
 # Writes file to replace line with vertex file number
-awk -v var="$number" 'NR==118 {$0="   structure_names = \"Lucidota_sp_"'"var"'"\"    //"} 1' template-input2d > input2d_w1_${i}
-awk -v var="$number" 'NR==119 {$0="   Lucidota_sp_"'"var"'" {   // "} 1' input2d_w1_${i} > input2d_w2_${i}
-awk -v var="$number" 'NR==221 {$0="   structure_names  = \"Lucidota_sp_"'"var"'"\"   //  "} 1' input2d_w2_${i} > input2d_w3_${i}
+awk -v var="$numspec" 'NR==118 {$0="   structure_names = \""'"var"'"\"    //"} 1' template-input2d > input2d_w1_${i}
+awk -v var="$numspec" 'NR==119 {$0="   "'"var"'" {   // "} 1' input2d_w1_${i} > input2d_w2_${i}
+awk -v var="$numspec" 'NR==221 {$0="   structure_names  = \""'"var"'"\"   //  "} 1' input2d_w2_${i} > input2d_w3_${i}
 # Sets Freq based on i
 speed=$(awk -v var="$i" 'NR==var' speed.txt)
 # Writes file to replace line with speed
@@ -34,7 +35,7 @@ awk -v var="$i" 'NR==261 {$0="   viz_dump_dirname = \"../results/ibamr/runs/viz_
 awk -v var="$i" 'NR==270 {$0="   data_dump_dirname = \"../results/ibamr/runs/hier_data_IB2d"'"var"'"\"          //"} 1' input2d_w7_${i} > input2d_w8_${i}
 
 # Cleans up folder
-mv input2d_w8_${i} "$WD"/data/input2d-files/input2d${i}
+mv input2d_w8_${i} "$WD"/data/input2d-files/"$Species"/input2d${i}
 rm input2d_w*_${i}
 
 
